322e2b712826d0acff95a5878b49d72bc45129e1
Dmitrii <dmitryc174@gmail.com>
2023-05-01 15:37:51 +0400

    Front controller and routes

--- www/index.php
+++ www/index.php
@@ -6,6 +6,33 @@ function myAutoLoader(string $className)
 
 spl_autoload_register('myAutoLoader');
 
+$route = $_GET['route'] ?? '';
+
+$route = $_GET['route'] ?? '';
+$routes = require __DIR__ . '/../src/routes.php';
+
+$isRouteFound = false;
+foreach ($routes as $pattern => $controllerAndAction) {
+    preg_match($pattern, $route, $matches);
+    if (!empty($matches)) {
+        $isRouteFound = true;
+        break;
+    }
+}
+
+if (!$isRouteFound) {
+    echo 'Страница не найдена!';
+    return;
+}
+unset($matches[0]);
+
+$controllerName = $controllerAndAction[0];
+$actionName = $controllerAndAction[1];
+
+$controller = new $controllerName();
+$controller->$actionName(...$matches);
+
+/*
 $controller = new \MyProject\Controllers\MainController();
 
 if (!empty($_GET['name'])) {
@@ -13,3 +40,4 @@ if (!empty($_GET['name'])) {
 } else {
     $controller->main();
 }
+*/
